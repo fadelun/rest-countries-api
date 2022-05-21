4daa6e23b2ecc57e86505bb0c5abe2fe5b2e8ff1
MuhammadFadll <fadeljourney@gmail.com>
2022-05-21 08:17:08 +0700

    fixed bug get countries

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,6 +12,7 @@ function Main() {
   const [result, setResult] = useState([]);
   const [regionName, setRegionName] = useState("");
 
+  // function untuk search
   const searchHandler = (searchCountry) => {
     setSearchCountry(searchCountry);
 
@@ -20,13 +21,14 @@ function Main() {
         return item.name.common.toLowerCase().includes(searchCountry.toLowerCase());
       });
       setResult(filteredCountries);
-      console.log(result);
+      console.log("result ", result);
     } else {
       setResult(countries);
-      console.log(countries);
+      console.log("Countries ", countries);
     }
   };
 
+  // function untuk filter
   const regionHandler = (item, regions) => {
     setRegionName(item);
     console.log(regions);
@@ -48,23 +50,58 @@ function Main() {
     }
   };
 
+  const sortHandler = (items) => {
+    items.sort(function (a, b) {
+      const nameA = a.name.common.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.common.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+  };
+
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((response) => response)
       .then((res) => {
         const datas = res.data;
+        sortHandler(datas);
+
+        console.log(datas);
+        // console.log(sortByName);
 
         setCountries(datas);
+        setResult(datas);
 
+        console.log("useEFfect ", result);
+        console.log("useEFfect ", countries);
         setLoading(false);
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err.message);
       });
-
-    console.log(result);
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://restcountries.com/v3.1/all")
+  //     .then((response) => response)
+  //     .then((res) => {
+  //       const datas = res.data;
+  //       setCountries(datas);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  //   console.log("useEffect-2 ", result);
+  // }, [regionName, result]);
 
   return (
     <main className=" w-4/5 xl:w-[1400px] min-h-screen pt-24 mx-auto relative ">
@@ -106,6 +143,33 @@ function Main() {
               </Link>
             );
           })
+          // countries &&
+          // countries.map((country, i) => {
+          //   return (
+          //     <Link key={i + 1} to={country.name.common.toLowerCase()}>
+          //       <div className="card bg-white shadow-md rounded-lg ">
+          //         <div className="image-container overflow-hidden rounded-t-lg  h-[166px] ">
+          //           <img src={country.flags.png} alt={country.name.common} className=" w-full object-cover lg:min-w-[264px] h-full" />
+          //         </div>
+          //         <div className="descirption px-6 py-7 font-semibold">
+          //           <h2 className="text-xl  mb-3">{country.name.common}</h2>
+
+          //           <p className="mb-1">
+          //             Population: <span className="font-normal">{FormatNumber(country.population)}</span>{" "}
+          //           </p>
+
+          //           <p className="mb-1">
+          //             Region:<span className="font-normal">{country.region}</span>{" "}
+          //           </p>
+
+          //           <p className="mb-1">
+          //             Capital: <span className="font-normal">{country.capital}</span>
+          //           </p>
+          //         </div>
+          //       </div>
+          //     </Link>
+          //   );
+          // })
         )}
       </div>
     </main>
